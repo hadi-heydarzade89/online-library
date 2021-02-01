f8a6fa0aac8629cca9b023ba3a92d7cef116bc20
Hadi <hadi.heydarzade@protonmail.com>
2021-02-02 00:00:41 +0330

    Add Author book Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AuthorSeeder::class);
         $this->call(BookSeeder::class);
         $this->call(BookingSeeder::class);
+        $this->call(AuthorBookSeeder::class);
     }
 }
